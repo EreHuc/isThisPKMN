14f2493e2e43162d40258ae72c20adafb030662e
EreHuc <huc.romain@gmail.com>
2015-11-25 19:47:19 +0100

    teste

---
+++ script.js
@@ -0,0 +1,36 @@
+creeMap();
+
+var red = new Image();
+red.src = "character.png";
+
+
+
+
+
+
+
+
+/*-----------------ZONE DE TEST-----------------*/
+// var d = 1;
+// while (d<17) {
+//   if(d<9){
+//     dessinerTile(d, ctx, (d*16)-16, 0);
+//   } else {
+//     dessinerTile(d, ctx, ((d-8)*16)-16, 16);
+//   }
+//   console.log(d);
+//   d++;
+// }
+
+/*ctx.drawImage(tuile, 200, 10);                      drawImage(image, dx, dy)
+ctx.drawImage(red, 200, 30, 100, 100);                drawImage(image, dx, dy, dw, dh)
+
+drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+ctx.drawImage(red, 0, 0, 16, 16, 0, 0, 16, 16);
+ctx.drawImage(red, 16, 0, 16, 16, 16, 0, 16, 16);
+ctx.drawImage(red, 0, 16, 16, 16, 0, 16, 16, 16);
+ctx.drawImage(red, 16, 16, 16, 16, 16, 16, 16, 16);
+ctx.drawImage(red, 0, 32, 16, 16, 0, 32, 16, 16);
+ctx.drawImage(red, 16, 32, 16, 16, 16, 32, 16, 16);
+ctx.drawImage(red, 0, 48, 16, 16, 0, 48, 16, 16);
+ctx.drawImage(red, 16, 32, 16, 16, 16, 48, 16, 16);*/
