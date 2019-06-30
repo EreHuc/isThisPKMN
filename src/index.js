b2f6c535a17d40ab0204183f405b8874b058a0ae
Erehuc <huc.romain@gmail.com>
2019-06-30 19:47:45 +0200

    Fix font and auto resize

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import { store } from './store';
 window.addEventListener('DOMContentLoaded', () => {
   initGame(store);
 
+  const handleSize = () => {
+    const gbcContainer = document.getElementById('gbc-container');
+    const {
+      offsetHeight: gbcScreenH,
+      offsetWidth: gbcScreenW,
+    } = document.getElementById('gbc-screen');
+    const { offsetHeight: bodyH, offsetWidth: bodyW } = document.body;
+
+    const scaleH = (Number(bodyH) * 0.83) / Number(gbcScreenH);
+    const scaleW = (Number(bodyW) * 0.75) / Number(gbcScreenW);
+
+    const scale = Math.min(scaleH, scaleW);
+
+    gbcContainer.setAttribute('style', `transform: scale(${scale});`);
+  };
+
+  window.addEventListener('resize', handleSize);
+  handleSize();
+
   // let sprites = [];
   // store.subscribe(() => {
   //   const { images } = store.getState();
